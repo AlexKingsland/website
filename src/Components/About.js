d43da456d7a58fefcdd5598775748d81271ad853
Alex Kingsland <alex.w.king@hotmail.com>
2021-01-15 10:18:58 -0500

    finalized website layout

---
+++ src/Components/About.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var profilepic= "images/"+this.props.data.image;
+      var bio1 = this.props.data.bio.statement1;
+      var bio2 = this.props.data.bio.statement2;
+      var bio3 = this.props.data.bio.statement3;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+      var resumeDownload = "downloads/"+this.props.data.resumedownload;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">       
+         <div className="four columns">
+            <img className="profile-pic"  src={profilepic} alt="Alex Kingsland Profile Pic" />
+         </div>
+         <div className="eight columns main-col">
+            <h2>About Me</h2>
+
+            <p>{bio1}</p>
+            <p>{bio2}</p>
+            <p>{bio3}</p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+						   <span>{name}</span><br />
+						   <span>	{city}, {state}
+                   </span><br />
+						   <span>{phone}</span><br />
+                     <span>{email}</span>
+					   </p>
+               </div>
+               <div className="columns download">
+                  <p>
+                     <a href={resumeDownload} target="_blank" className="button"><i className="fa fa-download"></i>View Resume</a>
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/Components/Resume.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+  render() {
+
+    if(this.props.data){
+      var skillmessage = this.props.data.skillmessage;
+      var education = this.props.data.education.map(function(education){
+        return <div className="row" key={education.school}>
+          <h3>{education.school}</h3>
+
+          <div className="eight columns">
+          
+          <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+          
+          <ul>
+          <li>{education.description1}</li>
+          <li>{education.description2}</li>
+          <li>{education.description3}</li>
+          </ul>
+          {/**
+          <div className="download">
+          <center>
+          <a href={"downloads/"+education.download} target="_blank" className="button"><i className="fa fa-download"></i>View Transcript</a>
+          </center>
+          </div>
+          **/}
+          </div>
+          <div className="four columns">  
+            <img className="resume-pic"  src={"images/"+education.image} alt="McMaster Logo" />       
+          </div>
+          
+        </div>
+        
+      })
+      var work = this.props.data.work.map(function(work){
+        return <div className="row" key={work.company}>
+          <h3>{work.company}</h3>
+          <div className="eight columns">
+            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+            <ul>
+            <li>{work.description1}</li>
+            <li>{work.description2}</li>
+            <li>{work.description3}</li>
+            </ul>
+            <div className="download">
+            <center>
+              <a href={"downloads/"+work.download} target="_blank" className="button"><i className="fa fa-download"></i>Employer Reference</a>
+            </center>
+            </div> 
+          </div>
+          <div className="four columns">  
+            <img className="resume-pic"  src={"images/"+work.image} alt="Workplace Logo" />
+                   
+          </div>
+          
+        </div>
+      })
+    }
+
+    return (
+      <section id="resume">
+
+      <div className="row education">
+         <div className="three columns header-col">
+            <h1><span>Education</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <div className="row item">
+               <div className="twelve columns">
+                 {education}
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <div className="row work">
+
+         <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+          {work}
+        </div>
+    </div>
+
+   </section>
+    );
+  }
+}
+
+export default Resume;
