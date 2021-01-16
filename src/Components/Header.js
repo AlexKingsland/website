ef52a991d802de1ffdaa3fcaea82170b9d8f0c6b
Alex Kingsland <alex.w.king@hotmail.com>
2021-01-16 16:10:48 -0500

    revised header scroll

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {background: ['url(images/background_pics/_V7A0512.JPG)',
+                                `url(images/background_pics/_V7A0793.jpg)`,
+                                `url(images/background_pics/laf.jpeg)`,
+                                `url(images/background_pics/rolls.jpeg)`,
+                                `url(images/background_pics/DJI_0030.jpg)`
+                                ],
+                  index: 0,
+                  total_images: 5};
+  }
+
+  next() { this.setState({index: (this.state.index+1)%this.state.total_images  }); }
+  prev() { this.setState({index: Math.abs(this.state.index-1)%this.state.total_images  }); }
+
   render() {
+
+
 
     if(this.props.data){
       var name = this.props.data.name;
@@ -9,55 +28,70 @@ class Header extends Component {
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var resumeDownload = "downloads/Alex_Kingsland_Resume_2020.pdf";
+      
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
+    
+
     }
 
-    return (
-      <header id="home">
+  return (
+    <header id="home" style={{background: this.state.background[this.state.index]}}>
 
-      <nav id="nav-wrap">
+    <nav id="nav-wrap">
 
-        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+      <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
-         <ul id="nav" className="nav">
-            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
-            <li><a className="smoothscroll" href="#about">About</a></li>
-	          <li><a className="smoothscroll" href="#resume">Resume</a></li>
-            <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
-            <li><a className="smoothscroll" href="#skills">Skills</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
-         </ul>
+       <ul id="nav" className="nav">
+          <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+          <li><a className="smoothscroll" href="#about">About</a></li>
+          <li><a className="smoothscroll" href="#resume">Resume</a></li>
+          <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
+          <li><a className="smoothscroll" href="#skills">Skills</a></li>
+          <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+          <li><a className="smoothscroll" href="#contact">Contact</a></li>
+       </ul>
 
-      </nav>
+    </nav>
 
-      <div className="row banner">
-         <div className="banner-text">
-            <h3>Hey! My name is</h3>
-            <h1 className="responsive-headline">{name}.</h1>
-            <h3>I'm a Toronto based <span>{occupation}</span> graduating April 2021.<br/> {description}.</h3>
-            <hr /> 
-            <ul className="social">
-               {networks}
-            </ul>
-            <center>
-            <div className="download">
-            <p>
-               <a href={resumeDownload} target="_blank" className="button"><i className="fa fa-download"></i>View Resume</a>
-            </p>
-            </div>
-            </center>
-         </div>
-      </div>
+    <div className="scroll-button">
+    <button className="button left" style={{outline: 'none', float: 'left'}} onClick={this.prev.bind(this)}>&#10094;</button>
+    </div>
+    <div className="row banner">
+       
+       <div className="banner-text">
+          <h3>Hey! My name is</h3>
+          <h1 className="responsive-headline">{name}.</h1>
+          <h3>I'm a Toronto based <span>{occupation}</span> graduating April 2021.<br/> {description}.</h3>
+          <hr /> 
+          <ul className="social">
+             {networks}
+          </ul>
+          <center>
+          <div className="download">
+          <p>
+             <a href={resumeDownload} target="_blank" className="button"><i className="fa fa-download"></i>View Resume</a>
+          </p>
+          </div>
+          </center>
+       </div>
+    </div>
+    <div className="scroll-button">
+    <button className="button right" style={{outline: 'none', float: 'right'}} onClick={this.next.bind(this)}>&#10095;</button>
+    </div>
+    
 
-      <p className="scrolldown">
-         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-      </p>
+    
+    <p className="scrolldown">
+       
+       <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+       
+    </p>
+    
 
-   </header>
+    </header>
     );
   }
 }
